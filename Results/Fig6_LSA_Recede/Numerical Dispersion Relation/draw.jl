f452dd6063d1f6e52ca440d7e485a0224f64de69
Alex Tam <alextam19@protonmail.com>
2023-06-12 19:56:19 +0930

    Add files via upload

--- Results/Fig6_LSA_Recede/Numerical Dispersion Relation/draw.jl
+++ Results/Fig6_LSA_Recede/Numerical Dispersion Relation/draw.jl
@@ -10,7 +10,8 @@ function draw_numerical()
     # Import data (κ,uf,γ)
     q = vec(readdlm("q-(-0.3,0.0,0.0).csv")); ω = vec(readdlm("omega-(-0.3,0.0,0.0).csv"))
     qn = [0, 2, 4, 6, 8, 10, 12, 14].*(π/10) # Numerical wave numbers
-    ωn = [0.0, 0.072984, 0.191081, 0.297859, 0.387747, 0.463990, 0.535317, 0.601232] # Numerical growth rates
+    # ωn = [0.0, 0.072984, 0.191081, 0.297859, 0.387747, 0.463990, 0.535317, 0.601232] # Numerical growth rates (Old)
+    ωn = [0.0, 0.073005, 0.191092, 0.297989, 0.387592, 0.463523, 0.534146, 0.601290] # Numerical growth rates
     # Plot data
     plot(q, ω, label = L"$\textrm{Theoretical}$", linecolor=:black, linewidth = 2, xlabel = L"$q$", ylabel = L"$\omega$", margin = 3mm, xlims = (0, 5.0), ylims = (-0.5,1.0), grid = true, legend=:bottomleft)
     scatter!(qn, ωn, label = L"$\textrm{Numerical}$", linecolor=:red, markercolor=:red, markersize = 6)
